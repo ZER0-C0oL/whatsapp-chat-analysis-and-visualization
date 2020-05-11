b6a7b87c20d8855a1ae159678fe6f9cf19586115
Mrudul Sankhere <32437824+ZER0-C0oL@users.noreply.github.com>
2020-05-11 12:02:50 +0530

    routing complete

--- script.js
+++ script.js
@@ -2,31 +2,34 @@ var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = './' + files.filetoupload.name;
-      const mv = require("mv");
-      mv(oldpath, newpath, function(err) {
-         if (err) {
-            console.log('> FileServer.jsx | route: "/files/upload" | err:', err);
-            throw err;
-        } 
-      });
-      const { exec } = require("child_process");
-      exec("python analyze_chat.py " + newpath);
-//      setTimeout(function(){}, 3000);
-//      var content = fs.readFileSync('analysis.html');
-//      res.end(content);
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
+http.createServer(function(req, res) {
+    if(req.url == '/'){
+      var content = fs.readFileSync('home.html');
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(content);   
+    }
+    else if(req.url == '/analyze'){
+       var form = new formidable.IncomingForm();
+       form.parse(req, function (err, fields, files) {
+          var oldpath = files.filetoupload.path;
+          var newpath = './uploads/' + files.filetoupload.name;
+          const mv = require("mv");
+          mv(oldpath, newpath, function(err) {
+             if (err) {
+                console.log('> FileServer.jsx | route: "/files/upload" | err:', err);
+                throw err;
+            } 
+          });
+          const { exec } = require("child_process");
+          exec("python analyze_chat.py " + newpath);
+//          res.writeHead(200, {'Content-Type': 'text/html'});
+//          var content = fs.readFileSync('button_page.html');
+//          res.end(content);
+           setTimeout(function(){
+               var content = fs.readFileSync('analysis.html');
+               res.writeHead(200, {'Content-Type': 'text/html'});
+               res.end(content); 
+           }, 3000);
+       });
+    }
 }).listen(8080);
